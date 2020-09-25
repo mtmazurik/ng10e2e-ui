1dd38f2e22f1520cab80a905de3ada8d63d0c8a9
Marty Mazurik <marty@martymazurik.com>
2020-09-25 14:52:15 -0700

    fix button alignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule} from '@angular/material/button'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MatDividerModule } from '@angular/material/divider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDividerModule
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
