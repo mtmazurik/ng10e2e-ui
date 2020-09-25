352a49d5a5d4c95fcb5e4626f9f659146792c248
Marty Mazurik <marty@martymazurik.com>
2020-09-25 11:43:46 -0700

    added lazy load button/route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'CreateAccount', loadChildren: () => import('./create-account/create-account.module').then(m => m.CreateAccountModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
